cf0b11c3f7e61ef1e6bafa42608f28e2f6da7304
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-05-28 21:55:01 +0300

    Update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   extends: ['airbnb-base', 'prettier'],
   rules: {
     'no-var': 'off',
+    'no-useless-concat': 'off',
     'prefer-destructuring': 'off',
     'prefer-arrow-callback': 'off',
+    'prefer-template': 'off',
     'no-param-reassign': ['error', { props: false }]
   },
   env: {
