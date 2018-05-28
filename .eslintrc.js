45dc0c73eea3e3b0d9cb77483c64a37843f24b97
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-05-28 14:02:54 +0300

    no-param-reassign (#19)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,9 @@ module.exports = {
     'no-var': 'off',
     'prefer-destructuring': 'off',
     'prefer-arrow-callback': 'off',
+    'no-param-reassign': ['error', { props: false }]
   },
   env: {
-    browser: true,
-  },
+    browser: true
+  }
 };
